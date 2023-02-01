6a2a8bcdcafca04b3e9586ddba467147f3591ce8
Anizair Lopes <anlopes123@gmail.com>
2023-01-31 22:07:35 -0300

    Feito teste para Domain Event

--- src/domain/event/@shared/event-dispatcher.spec.ts
+++ src/domain/event/@shared/event-dispatcher.spec.ts
@@ -13,11 +13,35 @@ describe("Domain Events tests", ()=>{
    it("Should unregister an event handler", () =>{
         const eventDispatcher = new EventDispatcher();
         const eventHandler = new SendEmailWhenProductIsCreatedHandler();
+   
         eventDispatcher.register("ProductCreatedEvent", eventHandler);
         expect(eventDispatcher.getEventHandlers["ProductCreatedEvent"][0]).toMatchObject(eventHandler);
+  
         eventDispatcher.unregister("ProductCreatedEvent", eventHandler);
         expect(eventDispatcher.getEventHandlers["ProductCreatedEvent"]).toBeDefined();
         expect(eventDispatcher.getEventHandlers["ProductCreatedEvent"].length).toBe(0);
 
    }); 
+
+   
+   it("Should unregister All an event handler", () =>{
+      const eventDispatcher = new EventDispatcher();
+      const eventHandler = new SendEmailWhenProductIsCreatedHandler();
+      const eventHandler1 = new SendEmailWhenProductIsCreatedHandler();
+      
+ 
+      eventDispatcher.register("ProductCreatedEvent", eventHandler); 
+      eventDispatcher.register("ProductCreatedEvent", eventHandler1);
+
+      
+      expect(eventDispatcher.getEventHandlers["ProductCreatedEvent"][0]).toMatchObject(eventHandler);
+      expect(eventDispatcher.getEventHandlers["ProductCreatedEvent"][1]).toMatchObject(eventHandler1);
+
+
+      eventDispatcher.unregisterAll();
+
+      expect(eventDispatcher.getEventHandlers["ProductCreatedEvent"]).toBeUndefined();
+
+
+ }); 
 });
--- src/domain/event/@shared/event-dispatcher.ts
+++ src/domain/event/@shared/event-dispatcher.ts
@@ -4,26 +4,33 @@ import EventInterface from "./event.interface";
 
 export default class EventDispatcher implements EventDispatcherInterface {
 
-    private eventHandlers:{ [eventName:string]: EventHandlerInterface[]} = {};
+    private eventHandlers: { [eventName: string]: EventHandlerInterface[] } = {};
 
-    get getEventHandlers():{ [eventName:string]: EventHandlerInterface[]} {
+    get getEventHandlers(): { [eventName: string]: EventHandlerInterface[] } {
         return this.eventHandlers;
     };
 
     notify(event: EventInterface): void {
-        
+
     };
-    register(eventName: string, eventHandler: EventHandlerInterface): void{
-        if(!this.eventHandlers[eventName]) {
+    register(eventName: string, eventHandler: EventHandlerInterface): void {
+        if (!this.eventHandlers[eventName]) {
             this.eventHandlers[eventName] = [];
         }
         this.eventHandlers[eventName].push(eventHandler);
     };
-    unregister(eventName: string, eventHandler: EventHandlerInterface): void{
+    unregister(eventName: string, eventHandler: EventHandlerInterface): void {
+       
+        if (this.eventHandlers[eventName]) {
+            const index = this.eventHandlers[eventName].indexOf(eventHandler);
+            if (index !== -1) {
+                this.eventHandlers[eventName].splice(index, 1);
+            }
+        }
 
     };
-    unregisterAll(): void{
-
+    unregisterAll(): void {
+        this.eventHandlers = {};
     };
 
 }
